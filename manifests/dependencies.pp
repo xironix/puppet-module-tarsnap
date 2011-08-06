f53f520245e0b83149d2030109d66dd4ed2b4cdf
Eivind Uggedal <eivind@uggedal.com>
2011-08-06 12:28:24 +0200

    Added missing dep manifest.

---
+++ manifests/dependencies.pp
@@ -0,0 +1,8 @@
+class tarnsap::dependencies {
+  $packages = ["build-essential",
+                "debhelper",
+                "e2fslibs-dev",
+                "libbz2-dev",
+                "zlib1g-dev",
+                "libssl-dev"]
+}
